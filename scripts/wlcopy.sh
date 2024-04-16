aff42aaa135775dff2d6ca91eeb5c4e13ec1dbf5
vipter2001 <vinayaksuresh39@gmail.com>
2024-04-16 19:45:15 +0530

    scripts added

---
+++ scripts/wlcopy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Prompt the user to enter the filename
+read -p "Enter the filename: " filename
+
+# Check if the file exists
+if [ ! -f "$filename" ]; then
+    echo "File '$filename' does not exist."
+    exit 1
+fi
+
+# Function to copy data to the clipboard
+copy_to_clipboard() {
+    wl-copy < "$1"
+    echo "Copied contents of '$1' to clipboard"
+}
+
+# Call the function with the provided filename
+copy_to_clipboard "$filename"
+
+# location to be pasted
+read -p "Enter your Folder Name(include too/):" folderloc
+read -p "file to be pasted:" filepaste
+#checking if  your folder and file  exist
+if [ ! -d "$2" ] && [ ! -f "$3" ]; then 
+   echo "Directory and file don't exist in '$2' and '$3'" 
+   exit 1
+fi
+
+
+#paste into me -_-
+
+wl-paste > "$filepaste"
+echo "Copied contents into '$filepaste'"
+
+
+
+
+
+
